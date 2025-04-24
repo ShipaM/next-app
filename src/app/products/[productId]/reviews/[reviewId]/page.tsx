503bdf08ba63669b45adc0edc93108579f13ac2f
Maksym Shypytsia <maksym.shypytsia@cd.p2h.com>
2025-04-24 20:49:46 +0300

    feat(files and folders in next.js): organizing files and folders in Next.js

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,16 @@
+import { notFound } from "next/navigation";
+
 export default async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
   const { productId, reviewId } = await params;
+
+  if (parseInt(reviewId) > 1000) {
+    return notFound();
+  }
+
   return (
     <h1>
       Review {reviewId} about product {productId}
